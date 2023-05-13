4e61e7d2530da9391ef87b2e4bec98bd81810b79
iosuAbal <jabal002@ikasle.ehu.eus>
2023-05-13 19:34:08 +0200

    current standings

--- app/src/main/java/com/example/futbolapp/DataAccess.java
+++ app/src/main/java/com/example/futbolapp/DataAccess.java
@@ -46,6 +46,47 @@ public class DataAccess {
     private static Match[] partidoakOrdenatuta;
 
 
+    public static CompletableFuture<Proba[]> getStandingsFromAPI(String league) {
+        CompletableFuture<Proba[]> future = new CompletableFuture<>();
+
+        String leagueId = String.valueOf(ApiMap.getValueByName(league));
+
+        OkHttpClient client = new OkHttpClient();
+        String url = "https://v3.football.api-sports.io/standings?league=" + leagueId + "&season=2022";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .addHeader("X-rapidapi-key", "4d1bac668930e231121a2980d1a049e4")
+                .build();
+        Call call = client.newCall(request);
+
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                System.out.println("Error in call");
+                future.completeExceptionally(e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String json = response.body().string();
+                System.out.println("Call done " + json);
+                try {
+                    JSONObject jsonObject = new JSONObject(json);
+                    JSONArray jsonArr = jsonObject.getJSONArray("response");
+                    Gson gson = new Gson();
+                    Proba[] standings = gson.fromJson(jsonArr.toString(), Proba[].class);
+
+                    future.complete(standings);
+                } catch (JSONException e) {
+                    System.out.println("Response ok but JSON ERROR");
+                    future.completeExceptionally(e);
+                }
+            }
+        });
+
+        return future;
+    }
     public static CompletableFuture<Match[]> getMatchesFromAPI(String league) {
         CompletableFuture<Match[]> future = new CompletableFuture<>();
 
--- app/src/main/java/com/example/futbolapp/ui/seasons/SeasonsFragment.java
+++ app/src/main/java/com/example/futbolapp/ui/seasons/SeasonsFragment.java
@@ -95,7 +95,7 @@ public class SeasonsFragment extends Fragment {
                 LinearLayout.LayoutParams.WRAP_CONTENT
         );
         params.setMargins(0, 20, 0, 0);
-        String urtea= selectedItem.split("-")[0];
+        String urtea= spinnerYears.getSelectedItem().toString().split("-")[0];
         String competi= spinnerCompetitions.getSelectedItem().toString();
         List<Match> partidoak;
         try {
